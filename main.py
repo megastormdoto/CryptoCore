e993e3f1f1a30090475bba00e10a76bcf12a1971
megastorm <dotamegastorm@gmail.com>
2025-11-11 17:58:29 +0300

    Sprint 1 update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ CryptoCore - Main Entry Point
 import sys
 import os
 
-# Добавляем путь к исходному коду
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'cryptocore', 'src'))
 
 from cryptocore import main
